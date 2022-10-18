3dbe96db385533f54103b560ce963ae69902ff9e
user <alyush47m@gmail.com>
2022-10-18 16:54:26 +0400

    init simple client, server chat

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 9999))
+
+
+def receive_data():
+    while True:
+        data = client_socket.recv(1024)
+        print(_decode(data))
+        send_data()
+
+
+def _decode(data: bytes):
+    return data.decode()
+
+
+def _encode(data: str):
+    return data.encode()
+
+
+def send_data():
+    data = input_message()
+    client_socket.send(_encode(data))
+
+
+def input_message():
+    data = input('data: ')
+    return data
+
+
+if __name__ == "__main__":
+    receive_data()
---
+++ server.py
@@ -0,0 +1,32 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 9999))
+server_socket.listen()
+
+
+def accept_connection():
+    while True:
+        client_socket, addr = server_socket.accept()
+        print('Connection from:', addr)
+        client_socket.send('hello'.encode())
+        receive_message(client_socket)
+
+
+def receive_message(client_socket: socket.socket):
+    while True:
+        data = client_socket.recv(1024)
+        print(data.decode())
+        if data:
+            send_message(client_socket)
+
+
+def send_message(client_socket: socket.socket):
+    data = 'Hello'
+    client_socket.send(data.encode())
+    print('message send')
+
+
+if __name__ == '__main__':
+    accept_connection()
